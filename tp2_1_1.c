4b7297e8e63d3ebf6b1a8aa4943b7e7876bd5bf4
vaninaze <vaninazee@gmail.com>
2023-03-30 16:47:39 -0300

    punto 2a

---
+++ tp2_1_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define N 20
+int main(){
+    int i;
+    double vt[N];
+    for(i = 0;i<N; i++)
+    {
+        vt[i]=1+rand()%100;
+        printf(" %.2f ", vt[i]);
+    }
+    return 0;
+}
